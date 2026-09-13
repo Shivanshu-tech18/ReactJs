323adc9f97eeb968f15d04a7a049fe51c35e144d
Shivanshu-tech18 <2k23.cs2312795@gmail.com>
2026-09-13 23:47:53 +0530

    Day !: React setup and first component

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Shivanshu Tripathi</h1>
+        <p>
+      B.Tech CSE Student
+        </p>
+      <p>
+        Learning React Day 1
+        </p> 
+          
+      </div>
+    </>
+  )
+}
+
+export default App
